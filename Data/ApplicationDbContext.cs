feb64a8f58de93eaaea4ed4c513c3672ba9bcd0d
DESKTOP-TNTQ7TC\mongo <mongo1248@yahoo.com>
2023-01-27 16:20:18 -0500

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeWebApp4.Models;
+
+namespace JokeWebApp4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeWebApp4.Models.Joke> Joke { get; set; }
+    }
+}
